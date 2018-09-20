2ef3157e256b6166edf352954123379d84dbf7fb
zzd <zdzheng12@gmail.com>
2018-09-19 17:40:04 -0700

    update folder

--- tripletfolder.py
+++ tripletfolder.py
@@ -22,13 +22,17 @@ class TripletFolder(datasets.ImageFolder):
         #camera_id = filename.split('_')[2][0:2]
         return int(camera_id)-1
 
-    def _get_pos_sample(self, target,index):
+    def _get_pos_sample(self, target, index):
         pos_index = np.argwhere(self.targets == target)
         pos_index = pos_index.flatten()
-        rand = random.randint(0,len(pos_index)-1)
-        if pos_index[rand] == index:
-            rand = random.randint(0,len(pos_index)-1)
-        return self.samples[pos_index[rand]]
+        pos_index = np.setdiff1d(pos_index, index)
+        rand = np.random.permutation(len(pos_index))
+        result_path = []
+        for i in range(4):
+           t = i%len(rand)
+           tmp_index = pos_index[rand[t]]
+           result_path.append(self.samples[tmp_index][0])
+        return result_path
 
     def _get_neg_sample(self, target):
         neg_index = np.argwhere(self.targets != target)
@@ -38,27 +42,27 @@ class TripletFolder(datasets.ImageFolder):
 
     def __getitem__(self, index):
         path, target = self.samples[index]
-        # cam = self.cams[index]
+        cam = self.cams[index]
         # pos_path, neg_path
-        pos_path, pos_target = self._get_pos_sample(target, index)
-        neg_path, neg_target = self._get_neg_sample(target)
-        neg_path2, neg_target2 = self._get_neg_sample(target)
+        pos_path = self._get_pos_sample(target, index)
 
         sample = self.loader(path)
-        pos = self.loader(pos_path)
-        neg = self.loader(neg_path)
-        neg2 = self.loader(neg_path2)
+        pos0 = self.loader(pos_path[0])
+        pos1 = self.loader(pos_path[1])
+        pos2 = self.loader(pos_path[2])
+        pos3 = self.loader(pos_path[3])
 
         if self.transform is not None:
             sample = self.transform(sample)
-            pos = self.transform(pos)
-            neg = self.transform(neg)
-            neg2 = self.transform(neg2)
+            pos0 = self.transform(pos0)
+            pos1 = self.transform(pos1)
+            pos2 = self.transform(pos2)
+            pos3 = self.transform(pos3)
 
         if self.target_transform is not None:
             target = self.target_transform(target)
-            pos_target = self.target_transform(pos_target)
-            neg_target = self.target_transform(neg_target)
-            neg_target2 = self.target_transform(neg_target2)
 
-        return sample, target, pos, pos_target, neg, neg_target, neg2, neg_target2
+        c,h,w = pos0.shape
+        pos = torch.cat((pos0.view(1,c,h,w), pos1.view(1,c,h,w), pos2.view(1,c,h,w), pos3.view(1,c,h,w)), 0)
+        pos_target = target
+        return sample, target, pos, pos_target
